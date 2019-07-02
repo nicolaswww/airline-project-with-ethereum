0c9c579353e51fd895e9f35ab96bdd49862237c3
Nicolás Acosta <nadesarrolloweb@gmail.com>
2019-07-02 18:31:34 -0300

    import and use web3 proxy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
 import Panel from "./Panel";
+import getWeb3 from './getWeb3';
+import AirlineContract from './airline';
 
 export class App extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            account: undefined
+        };
+    }
+
+    async componentDidMount() {
+        this.web3 = await getWeb3();
+        this.airline = await AirlineContract(this.web3.currentProvider);
+        
+        const account = (await this.web3.eth.getAccounts())[0];
+
+        this.setState({
+            account: account.toLowerCase()
+        }, () => {
+            this.load();
+        });
+    }
+
+    async load() {
+
     }
 
     render() {
