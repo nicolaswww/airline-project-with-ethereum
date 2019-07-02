9b30b5253859ffb5ecce4b403f07075851bd94a8
Nicolás Acosta <nadesarrolloweb@gmail.com>
2019-07-02 18:56:23 -0300

    add airline service with utils

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import React, { Component } from "react";
 import Panel from "./Panel";
 import getWeb3 from './getWeb3';
 import AirlineContract from './airline';
+import { AirlineService } from './airlineService';
+
+const converter = (web3) => {
+    return (value) => {
+        return web3.utils.fromWei(value.toString(), 'ether');
+    }
+}
 
 export class App extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            account: undefined
+            account: undefined,
+            balance: 0
         };
     }
 
     async componentDidMount() {
         this.web3 = await getWeb3();
+        this.toEther = converter(this.web3);
         this.airline = await AirlineContract(this.web3.currentProvider);
+        this.airlineService = new AirlineService(this.airline);
         
         const account = (await this.web3.eth.getAccounts())[0];
 
@@ -25,8 +35,15 @@ export class App extends Component {
         });
     }
 
-    async load() {
+    async getBalance() {
+        let weiBalance = await this.web3.eth.getBalance(this.state.account);
+        this.setState({
+            balance: this.toEther(weiBalance)
+        });
+    }
 
+    async load() {
+        this.getBalance();
     }
 
     render() {
@@ -38,7 +55,8 @@ export class App extends Component {
             <div className="row">
                 <div className="col-sm">
                     <Panel title="Balance">
-
+                        <p><strong>{ this.state.account }</strong></p>
+                        <span><strong>Balance:</strong> { this.state.balance }</span>
                     </Panel>
                 </div>
                 <div className="col-sm">
---
+++ src/airlineService.js
@@ -0,0 +1,30 @@
+export class AirlineService {
+    constructor(contract) {
+        this.contract = contract;
+    }
+
+    async getFlights() {
+        let total = await this.getTotalFlights();
+        let flights = [];
+
+        for(var i = 0; i < total; i++) {
+            let flight = await this.contract.flights(i);
+            flights.push(flight);
+        }
+
+        return this.mapFlights(flights);
+    }
+
+    async getTotalFlights() {
+        return await (this.contract.totalFlights()).toNumber();
+    }
+
+    mapFlights(flights) {
+        return flights.map(flight => {
+            return {
+                name: flight[0],
+                price: flight[1].toNumber()
+            };
+        });
+    }
+}
